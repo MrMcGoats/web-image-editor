3362004fd8ed68c7e3443b887ff57fc8953527df
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2024-05-13 20:12:10 -0700

    Can now upload images and move them around the screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::collections::HashMap;
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
+
 use yew::prelude::*;
-use web_sys::{MouseEvent, console, HtmlElement};
-use gloo_events::EventListener;
+use web_sys::{MouseEvent, console, HtmlElement, HtmlInputElement, FileList};
+use gloo::file::File;
+use gloo::file::callbacks::FileReader;
 
 #[derive(PartialEq, Properties)]
 struct Props {
@@ -9,36 +14,111 @@ struct Props {
 	children: Children,
 }
 
-#[function_component]
-fn App() -> Html {
-	let counter = use_state(|| 0);
-	let onclick = {
-		let counter = counter.clone();
-		move |_| {
-			let value = *counter + 1;
-			counter.set(value);
+struct FileDetails {
+	name: String,
+	file_type: String,
+	data: Vec<u8>,
+}
+
+pub enum Msg {
+	Loaded(String, String, Vec<u8>),
+	Files(Vec<File>)
+}
+
+pub struct App {
+	readers: HashMap<String, FileReader>,
+	files: Vec<FileDetails>,
+}
+
+impl Component for App {
+	type Message = Msg;
+	type Properties = ();
+
+	fn create(_ctx: &Context<Self>) -> Self {
+		Self {
+			readers: HashMap::default(),
+			files: Vec::default(),
 		}
-	};
+	}
 
-	let onclickminus = {
-		let counter = counter.clone();
-		move |_| {
-			let value = *counter - 1;
-			counter.set(value);
+	fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+		match msg {
+			Msg::Loaded(file_name, file_type, data) => {
+				self.files.push(FileDetails {
+					name: file_name.clone(),
+					file_type,
+					data,
+				});
+				self.readers.remove(&file_name);
+				true
+			}
+			Msg::Files(files) => {
+				for file in files.into_iter() {
+					let file_name = file.name();
+					let file_type = file.raw_mime_type();
+
+					let task = {
+						let link = ctx.link().clone();
+						let file_name = file_name.clone();
+
+						gloo::file::callbacks::read_as_bytes(&file, move |res| {
+							link.send_message(Msg::Loaded(
+									file_name,
+									file_type,
+									res.expect("failed to read file"),
+							  ))
+						})
+					};
+					self.readers.insert(file_name, task);
+				}
+				true
+			}
 		}
-	};
+	}
 
-
-	html! {
-		<div>
-			<MouseMoveComponent id="mousebox" style="background-color: red;">
-				<button {onclick}>{ "+1" }</button>
-				<button onclick={onclickminus}>{ "-1" }</button>
-				<p>{ *counter }</p>
-			</MouseMoveComponent>
-		</div>
+	fn view(&self, ctx: &Context<Self>) -> Html {
+		html! {
+			<div>
+				<input id="file-upload" type="file" accept="image/*" multiple={true}
+					onchange={ctx.link().callback(move |e: Event| {
+						let input: HtmlInputElement = e.target_unchecked_into();
+						Self::upload_files(input.files())
+					})} />
+				<MouseMoveComponent id="photo_move" style="">
+					{ for self.files.iter().map(App::view_file) }
+				</MouseMoveComponent>
+			</div>
+		}
 	}
 }
+
+impl App {
+	fn view_file(file: &FileDetails) -> Html {
+		html! {
+			<div class="preview-tile">
+				<p class="preview-name">{ format!("{}", file.name) }</p>
+				<div class="preview-media">
+					<img src={ format!("data:{};base64,{}", file.file_type, STANDARD.encode(&file.data)) } type={file.file_type.clone()} />
+				</div>
+			</div>
+		}
+	}
+
+	fn upload_files(files: Option<FileList>) -> Msg {
+		let mut result = Vec::new();
+
+		if let Some(files) = files {
+			let files = js_sys::try_iter(&files)
+				.unwrap()
+				.unwrap()
+				.map(|v| web_sys::File::from(v.unwrap()))
+				.map(File::from);
+			result.extend(files);
+		}
+		Msg::Files(result)
+	}
+}
+
 
 #[function_component]
 fn MouseMoveComponent(props: &Props) -> Html {
