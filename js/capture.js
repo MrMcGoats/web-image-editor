82622717db36d6551f5da23eaa4b4d197f0b5694
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2024-05-18 18:41:40 -0700

    Saving will now remove canvas border and save file to user's computer

--- js/capture.js
+++ js/capture.js
@@ -1,5 +1,21 @@
 function capture(id) {
+  // Remove borders from the captured element
+  const old_border = document.querySelector(id).style.border;
+  document.querySelector(id).style.border = 'none';
+
   html2canvas(document.querySelector(id)).then(canvas => {
     document.body.appendChild(canvas)
+    
+    // Restore the border
+    document.querySelector(id).style.border = old_border;
+
+    // Save the image
+    const dataURL = canvas.toDataURL();
+    const a = document.createElement('a');
+    a.href = dataURL;
+    a.download = 'image.png';
+    a.click();
+    document.body.removeChild(canvas);
+    document.body.removeChild(a);
   });
 }
