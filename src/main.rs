07586dc151746bb8a313fc21b9e81777b9e55ebf
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2024-05-27 12:08:56 -0700

    Can now use as part of any web page

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ pub mod file_details;
 pub mod text_details;
 pub mod page_items;
 
+use wasm_bindgen::prelude::*;
 use web_sys::console;
 use app::App;
 
 fn main() {
+	#[cfg(feature = "standalone")]
 	yew::Renderer::<App>::new().render();
+}
+
+#[cfg(not(feature = "standalone"))]
+#[wasm_bindgen(start)]
+pub fn run_app() {
+	let document = web_sys::window().unwrap().document().unwrap();
+	let root = document.get_element_by_id("canvas-root").unwrap();
+	console::log_1(&"Starting app".into());
+	yew::Renderer::<App>::with_root(root).render();
 }
